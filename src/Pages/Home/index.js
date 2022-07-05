d9044fc1c18669b03ea1aa181334201cf3c7d8f4
Rodrigo Alves <rodrigo.gois@live.com>
2022-07-05 17:23:37 -0300

    added: new home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -17,12 +17,20 @@ const Home = () => {
       <Container>
         <Row>
           <Col md={7}>
-            <h2>Como avaliar um candidato?</h2>
+            <p>
+              Muitas vezes, nós como leigos, acabamos não sabemos como avaliar um candidato. 
+              São diversos fatores, dados e nem sempre sabemos onde procurar, ou como analisá-los.
+              E no momento de tomar uma decisão, não basta ter opinião. A gente pode estar enviesado por um momento bom, ou ruim. 
+              E já que estamos falando de um governo, podemos nos deixar influenciar com medidas em período de eleição:
+            </p>
+            <ul>
+              <li>Aquela estação nova que foi inaugurada é ótima, <b><i>mas está atrasada a 10 anos</i></b></li>
+              <li>Nossa, o dólar está abaixando, <b><i>mas o governo pode estar injetando para frear a consequencia de um escandalo</i></b></li>
+            </ul>
+            <h2>Mas como avaliar posso avaliar candidato?</h2>
             <p>
               No período de eleição candidatos disponibilizam seu plano de governo, mas isso não garente que ele irá cumprir tudo que promete.
-              Se não bastasse, esse materiais tem como objetivo vender o candido e nos lubridiar.
-            </p>
-            <p>
+              Se não bastasse, esse materiais tem como objetivo vender o candido e nos lubridiar.<br/>
               Por outro lado, um candido que já esteve a frente de um governo já nos mostrou seus interesses e o 
               quanto foi efetivo no cumprimento de seu dever.
             </p>
@@ -32,14 +40,19 @@ const Home = () => {
           <Col md={10}>
             <h2>Números não mentem</h2>
             <p>
-              Candidatos disponibilizam seu plano de governo, mas isso não garente que ele irá cumprir tudo que promete.
+              Os indicadores da economia são uma excelente indicador de qualidade de um governo.
             </p>
             <p>
-              Apesar disso, um candido que já esteve a frente de um governo já nos mostrou seus interesses e o 
-              quanto foi efetivo no cumprimento de seu dever.
+              <b>"Tá, mas e a Covid? E o estouro da bolha imobiliária americana?"</b>
+            </p>
+            <p>
+              Fatores externos ao governo, sejam positivos ou negativos, podem ser indificados no gráficos. Mas estes são um grão de areia.
             </p>
 
             <h2>Dados simples</h2>
+            <p>
+              <b>"Bom, mas e aí? Como posso analisar os dados?"</b>
+            </p>
             <p>
               Buscamos trazer indicadores comuns que afetam seu dia-a-dia e compara-los com outros que são relacionados, de forma a facilitar sua análise.
             </p>
