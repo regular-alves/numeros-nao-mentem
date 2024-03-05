181d9d36708530922ab8949363099c493aa5ac4a
regular-alves <rodrigo.gois@live.com>
2024-03-05 11:44:55 +0000

    added: tests to useDatesFromPath

--- src/hooks/useDatesFromPath.test.ts
+++ src/hooks/useDatesFromPath.test.ts
@@ -2,15 +2,50 @@ import {expect, test} from '@jest/globals';
 import DateToString from "../utils/DateToString";
 import { getDatesFrom } from "./useDatesFromPath";
 
+const sixMonthsAgo = new Date();
+const today = new Date();
+
+sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6)
+
+test('Path dates should match to "from" and "to" params', () => {
+  const { from, to } = getDatesFrom(`/reports/example/${DateToString(sixMonthsAgo, 'Y-m')}/${DateToString(today, 'Y-m')}`);
+
+  expect(DateToString(from, 'Y-m')).toBe(DateToString(sixMonthsAgo, 'Y-m'));
+  expect(DateToString(to, 'Y-m')).toBe(DateToString(today, 'Y-m'));
+});
 
 test('Empty path should return Today and 6 months before', () => {
-  const today = new Date();
-  const sixMonthsAgo = new Date();
-
-  sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6)
-
   const { from, to } = getDatesFrom('/reports/example');
 
-  expect(DateToString(from)).toBe(DateToString(sixMonthsAgo));
-  expect(DateToString(to)).toBe(DateToString(today));
+  expect(DateToString(from, 'Y-m')).toBe(DateToString(sixMonthsAgo, 'Y-m'));
+  expect(DateToString(to, 'Y-m')).toBe(DateToString(today, 'Y-m'));
+});
+
+test('Single date path should append today date', () => {
+  const { from, to } = getDatesFrom(`/reports/example/${DateToString(sixMonthsAgo, 'Y-m')}`);
+
+  expect(DateToString(from, 'Y-m')).toBe(DateToString(sixMonthsAgo, 'Y-m'));
+  expect(DateToString(to, 'Y-m')).toBe(DateToString(today, 'Y-m'));
+});
+
+test('Inverted period should be correctly setted', () => {
+  const { from, to } = getDatesFrom(`/reports/example/${DateToString(today, 'Y-m')}/${DateToString(sixMonthsAgo, 'Y-m')}`);
+
+  expect(DateToString(from, 'Y-m')).toBe(DateToString(sixMonthsAgo, 'Y-m'));
+  expect(DateToString(to, 'Y-m')).toBe(DateToString(today, 'Y-m'));
+});
+
+test('More than two dates, ignores intermediare values', () => {
+  const anotherDate = new Date();
+
+  anotherDate.setMonth(anotherDate.getDate() - 3);
+
+  const { from, to } = getDatesFrom(
+    '/reports/example/' +
+    `${DateToString(today, 'Y-m')}/` +
+    `${DateToString(sixMonthsAgo, 'Y-m')}/` +
+    `${DateToString(anotherDate, 'Y-m')}`);
+
+  expect(DateToString(from, 'Y-m')).toBe(DateToString(sixMonthsAgo, 'Y-m'));
+  expect(DateToString(to, 'Y-m')).toBe(DateToString(today, 'Y-m'));
 });
