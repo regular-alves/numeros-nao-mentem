28bc24c3663f3b9d24ec5000d0505b82227efee1
regular-alves <rodrigo.gois@live.com>
2024-07-30 22:43:43 +0000

    refac: adjusting attribute to be friendier

--- src/iterable/Records.ts
+++ src/iterable/Records.ts
@@ -2,5 +2,5 @@ import TupleIterable from "./Tuple";
 import Record from "../dtos/Record";
 
 export default class Records<T extends Record> extends TupleIterable<T> {
-  protected monthInSeconds = 2592000;
+  protected monthInSeconds = 60 * 60 * 24 * 31;
 }
