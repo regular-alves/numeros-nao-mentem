0cad4fb0b68be685fedf852ebc2c07ccfc08bef7
regular-alves <rodrigo.gois@live.com>
2024-07-31 23:25:12 +0000

    feat: implementing method to access keys

--- src/iterable/Tuple.ts
+++ src/iterable/Tuple.ts
@@ -11,6 +11,14 @@ export default class Tuple<T> implements Iterable<T> {
     return new TupleIterator(this.values);
   }
 
+  index(i: number): T {
+    if (typeof this.values[i] === 'undefined') {
+      throw new Error(`Index ${i} does not exist`);
+    }
+
+    return this.values[i];
+  }
+
   some(predicate: (val: T, index: number, array: T[]) => boolean): boolean {
     return this.values.some(predicate);
   }
