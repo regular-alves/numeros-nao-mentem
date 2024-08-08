81482728340bba60e646423ec85c9435259e4182
regular-alves <rodrigo.gois@live.com>
2024-08-08 22:38:08 +0000

    rm: useless object

--- src/iterable/Records.ts
+++
@@ -1,6 +0,0 @@
-import TupleIterable from "./Tuple";
-import Record from "../dtos/Record";
-
-export default class Records<T extends Record> extends TupleIterable<T> {
-  protected monthInSeconds = 60 * 60 * 24 * 31;
-}
