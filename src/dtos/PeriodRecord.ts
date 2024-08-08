8db4df2121addc21e41685124cb11dbb4363cbd3
regular-alves <rodrigo.gois@live.com>
2024-08-08 22:37:37 +0000

    refac: adding condition to set value to optional prop

--- src/dtos/PeriodRecord.ts
+++ src/dtos/PeriodRecord.ts
@@ -10,10 +10,13 @@ export default class PeriodRecord extends Record {
   readonly start: Date;
   readonly end?: Date;
 
-  constructor(value: number, start: Date, end?: Date) {
+  constructor(value: number, start: Date, end: Date | null | undefined = null) {
       super(value);
 
       this.start = start;
-      this.end = end;
+
+      if (end) {
+        this.end = end;
+      }
   }
 }
